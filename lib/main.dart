f31bd18d9861dca946db9ac7fa605cd5d28bf4b1
Mohamad-Ammis <esd676q8@gmail.com>
2024-09-27 16:57:19 -0700

    add stripe packages

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_stripe/flutter_stripe.dart';
 import 'package:stripe_integration/core/utils/app_routes.dart';
 
 void main() {
@@ -16,3 +17,31 @@ class StripePayment extends StatelessWidget {
     );
   }
 }
+//1. CREATE PAYMENT INTENT WITH REQUIRD PARAM {AMOUNT , CUREENCY}
+//2. INTEGRATE PAYMENT SHEET WITH REQUIRED PARAM {merchantDisplayName,paymentIntentClientSecret}
+// Future<void> initPaymentSheet() async {
+//     try {
+//       // 1. create payment intent on the server
+//       final data = await _createTestPaymentSheet();
+
+//       // 2. initialize the payment sheet
+//      await Stripe.instance.initPaymentSheet(
+//         paymentSheetParameters: SetupPaymentSheetParameters(
+//           // Set to true for custom flow
+//           customFlow: false,
+//           // Main params
+//           merchantDisplayName: 'Flutter Stripe Store Demo',
+//           paymentIntentClientSecret: data['paymentIntent'],
+//         ),
+//       );
+//       setState(() {
+//         _ready = true;
+//       });
+//     } catch (e) {
+//       ScaffoldMessenger.of(context).showSnackBar(
+//         SnackBar(content: Text('Error: $e')),
+//       );
+//       rethrow;
+//     }
+// }
+//3. PRESENT PAYMENT SHEET 
